55eb3ee753d72d06d11490fc238dd1087666376a
Manoj Bisht <mbisht@MacBook-Pro-32.local>
2018-08-08 12:30:42 +0100

    Add: route configuration

--- src/components/homePage.js
+++ src/components/homePage.js
@@ -1,13 +1,16 @@
 "use strict";
 
 var React = require('react');
+var Router = require('react-router');
+var Link = Router.Link;
 
 var Home = React.createClass({
 	render: function () {
 		return (
 			<div className="jumbotron">
 				<h1>Welcome to NCSC</h1>
-				<p>React, React Router, and Flux for ultra-responsive web apps.</p>
+				<p>The NCSC was set up to help protect our critical services from cyber attacks, managing major incidents and improve the underlying security of the UK Internet through technological improvement and advice to citizens and organisations. Our vision is to help make the UK the safest place to live and do business online.</p>
+				<Link to="about" className="btn btn-primary btn-lg">Learn more</Link>
 			</div>
 		);
 	}
