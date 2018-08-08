38dd3332727a8866383653aea70a136540c4a942
Manoj Bisht <mbisht@MacBook-Pro-32.local>
2018-08-08 10:15:07 +0100

    ADD: Mock API Setup

---
+++ src/api/authorData.js
@@ -0,0 +1,20 @@
+module.exports = {
+	authors: 
+	[
+		{
+			id: 'Meltdown and Spectre', 
+			firstName: 'MJ', 
+			lastName: 'B'
+		},	
+		{
+			id: 'Cyberfirst', 
+			firstName: 'Scott', 
+			lastName: 'Allen'
+		},	
+		{
+			id: 'Cyberessentials', 
+			firstName: 'Ross', 
+			lastName: 'Wahlin'
+		}
+	]
+};
--- src/components/homePage.js
+++ src/components/homePage.js
@@ -3,9 +3,9 @@
 var React = require('react');
 
 var Home = React.createClass({
-    render: function() {
-        return (
-		    <div className="jumbotron">
+	render: function () {
+		return (
+			<div className="jumbotron">
 				<h1>Welcome to NCSC</h1>
 				<p>React, React Router, and Flux for ultra-responsive web apps.</p>
 			</div>
